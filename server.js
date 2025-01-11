f061cdfecd31f9dbfefcc39d1aa34aa6ca6a8637
ABISHEK M K <abishek.mk.01@gmail.com>
2025-01-11 19:12:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const upload = multer({
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.js'));
 });
 
 app.get('/music', (req, res) => {
